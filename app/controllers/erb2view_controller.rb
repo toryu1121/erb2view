957036d657b1417779fe235578d9802f53954d62
toryu1121 <y28bdgw@gmail.com>
2017-10-23 14:23:36 +0000

    erb03

--- app/controllers/erb2view_controller.rb
+++ app/controllers/erb2view_controller.rb
@@ -1,11 +1,32 @@
 class Erb2viewController < ApplicationController
   def index
+    @test10d_list = Erb2view.order("created_at DESC").limit(5)
   end
 
   def top
   end
 
   def input
+    @test10d_list = Erb2view.order("created_at DESC").limit(5)
+    
+    p "#####okkkk#######"
+    @ok = Erb2view.new do |i|
+      tom = params.permit(:text)["text"]
+      p tom
+      i.text = tom
+      i.save
+    end
+    
+    p "############"
+    @ok = respond_to do |format|
+      if @ok.save
+        p"########save##############"
+        format.js
+      end
+    end
+    
+    
+    
   end
 
   def view
